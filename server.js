51b0424a5da457d9957c27acd6e881cf0fe67dd2
Anton Zvertanovsky <anton.zv2002@gmail.com>
2023-05-25 19:00:12 +0300

    fetch/  checking the operation of the DB_HOST key

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
 const app = require('./app');
+const {DB_HOST} = process.env;
 
+mongoose.set('strictQuery', true);
 
-app.listen(3000, () => {
-	console.log("Server running. Use our API on port: 3000");
-});
+mongoose
+   .connect(DB_HOST)
+   .then(() => {
+      app.listen(3000);
+   })
+   .catch(error => {
+      console.log(error.message);
+      process.exit(1);
+   });
