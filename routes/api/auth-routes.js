fa5da88263fbaad6551a47d807d25a2c602c6329
Anton Zvertanovsky <anton.zv2002@gmail.com>
2023-06-07 17:17:15 +0300

    fetch/ adding a standard avatar to the user, the ability to change the avatar

--- routes/api/auth-routes.js
+++ routes/api/auth-routes.js
@@ -1,9 +1,8 @@
 const express = require("express");
 
 
-const {register, login, getCurrent, logout} = require('../../controllers/auth-controllers');
-const {authenticate} = require("../../middleware")
-
+const {register, login, getCurrent, logout, updateAvatar} = require('../../controllers/auth-controllers');
+const {authenticate, upload} = require("../../middleware")
 
 const router = express.Router();
 
@@ -11,5 +10,6 @@ router.post("/register", register);
 router.post("/login", login);
 router.get("/current", authenticate, getCurrent);
 router.post("/logout", authenticate, logout);
+router.patch('/avatars', authenticate, upload.single("avatar"), updateAvatar);
 
 module.exports = router;
